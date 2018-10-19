47989536316cf034fd600a8b4453a07bf7636e03
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2018-10-19 12:10:45 +0800

    優化代碼啊//

--- Lottery/app/src/main/java/com/international/wtw/lottery/activity/mine/BetrecordActivity.java
+++ Lottery/app/src/main/java/com/international/wtw/lottery/activity/mine/BetrecordActivity.java
@@ -2,10 +2,12 @@ package com.international.wtw.lottery.activity.mine;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.international.wtw.lottery.R;
 import com.international.wtw.lottery.adapter.BetdatasAdapter;
 import com.international.wtw.lottery.api.HttpCallback;
@@ -15,9 +17,7 @@ import com.international.wtw.lottery.base.NewBaseActivity;
 import com.international.wtw.lottery.newJason.BetrecordBydateModel;
 import com.international.wtw.lottery.newJason.WeekdateModel;
 import com.international.wtw.lottery.utils.SharePreferencesUtil;
-
 import java.math.BigDecimal;
-
 import butterknife.BindView;
 import butterknife.OnClick;
 
@@ -29,6 +29,14 @@ public class BetrecordActivity extends NewBaseActivity {
     TextView tvBetMoney;
     @BindView(R.id.tv_win_money)
     TextView tvWinMoney;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.ll_title)
+    LinearLayout llTitle;
+    @BindView(R.id.iv_empty)
+    ImageView ivEmpty;
+    @BindView(R.id.ll_bottom)
+    LinearLayout llBottom;
 
     private int betMoney;
     BigDecimal totalWinmoney = new BigDecimal(0);
@@ -49,7 +57,7 @@ public class BetrecordActivity extends NewBaseActivity {
 
             @Override
             public void onFailure(String msgCode, String errorMsg) {
-                Toast.makeText(BetrecordActivity.this,"请求数据失败，请重试！",Toast.LENGTH_SHORT).show();
+                Toast.makeText(BetrecordActivity.this, "请求数据失败，请重试！", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -59,18 +67,25 @@ public class BetrecordActivity extends NewBaseActivity {
         HttpRequest.getInstance().getBetReportByDate(this, token, beginTime, endTime, new HttpCallback<BetrecordBydateModel>() {
             @Override
             public void onSuccess(BetrecordBydateModel data) throws Exception {
-                BetdatasAdapter adapter = new BetdatasAdapter(BetrecordActivity.this, data.getData());
-                lvListdata.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
-
-                int size = data.getData().size();
-                for (int i = 0; i < size; i++) {
-                    betMoney += data.getData().get(i).getAmount();
-                    totalWinmoney = totalWinmoney.add(new BigDecimal(String.valueOf(data.getData().get(i).getWinLoseAmount())));
+                if (data.getData().size()==0){
+                    llTitle.setVisibility(View.GONE);
+                    ivEmpty.setVisibility(View.VISIBLE);
+                    llBottom.setVisibility(View.GONE);
+                }else{
+                    llTitle.setVisibility(View.VISIBLE);
+                    ivEmpty.setVisibility(View.GONE);
+                    llBottom.setVisibility(View.VISIBLE);
+                    BetdatasAdapter adapter = new BetdatasAdapter(BetrecordActivity.this, data.getData());
+                    lvListdata.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    int size = data.getData().size();
+                    for (int i = 0; i < size; i++) {
+                        betMoney += data.getData().get(i).getAmount();
+                        totalWinmoney = totalWinmoney.add(new BigDecimal(String.valueOf(data.getData().get(i).getWinLoseAmount())));
+                    }
+                    tvBetMoney.setText("下注金额:" + betMoney);
+                    tvWinMoney.setText("输赢金额:" + totalWinmoney);
                 }
-
-                tvBetMoney.setText("下注金额:" + betMoney);
-                tvWinMoney.setText("输赢金额:" + totalWinmoney);
             }
 
             @Override
@@ -86,10 +101,9 @@ public class BetrecordActivity extends NewBaseActivity {
     }
 
 
-
-
     @OnClick(R.id.iv_back)
     public void onViewClicked() {
         finish();
     }
+
 }
--- Lottery/app/src/main/java/com/international/wtw/lottery/activity/mine/SettledOrdersActivity.java
+++ Lottery/app/src/main/java/com/international/wtw/lottery/activity/mine/SettledOrdersActivity.java
@@ -2,7 +2,9 @@ package com.international.wtw.lottery.activity.mine;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +19,7 @@ import com.international.wtw.lottery.newJason.SettledOrdersModel;
 import com.international.wtw.lottery.utils.SharePreferencesUtil;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SettledOrdersActivity extends NewBaseActivity {
@@ -28,6 +31,12 @@ public class SettledOrdersActivity extends NewBaseActivity {
     TextView tvBetMoney;
     @BindView(R.id.tv_win_money)
     TextView tvWinMoney;
+    @BindView(R.id.ll_title)
+    LinearLayout llTitle;
+    @BindView(R.id.iv_empty)
+    ImageView ivEmpty;
+    @BindView(R.id.ll_bottom)
+    LinearLayout llBottom;
 
     @Override
     protected int getLayoutId() {
@@ -40,14 +49,26 @@ public class SettledOrdersActivity extends NewBaseActivity {
         HttpRequest.getInstance().getTodaySettledOrders(this, token, "", "", 1, 10, new HttpCallback<SettledOrdersModel>() {
             @Override
             public void onSuccess(SettledOrdersModel data) throws Exception {
-                SettledOrderAdapter adapter=new  SettledOrderAdapter(SettledOrdersActivity.this,data.getData().getData());
-                lvListdata.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
+                if (data.getData().getData().size() == 0) {
+                    llTitle.setVisibility(View.GONE);
+                    ivEmpty.setVisibility(View.VISIBLE);
+                    llBottom.setVisibility(View.GONE);
+                } else {
+                    llBottom.setVisibility(View.VISIBLE);
+                    llTitle.setVisibility(View.VISIBLE);
+                    ivEmpty.setVisibility(View.GONE);
+                    tvBetMoney.setText("下注金额: " + data.getData().getAmount());
+                    tvWinMoney.setText("輸贏金額: " + data.getData().getWinamount());
+                    SettledOrderAdapter adapter = new SettledOrderAdapter(SettledOrdersActivity.this, data.getData().getData());
+                    lvListdata.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+
             }
 
             @Override
             public void onFailure(String msgCode, String errorMsg) {
-                Toast.makeText(SettledOrdersActivity.this,"请求数据失败.",Toast.LENGTH_SHORT).show();
+                Toast.makeText(SettledOrdersActivity.this, "请求数据失败.", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -58,9 +79,15 @@ public class SettledOrdersActivity extends NewBaseActivity {
         return false;
     }
 
-
     @OnClick(R.id.iv_back)
     public void onViewClicked() {
         finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
